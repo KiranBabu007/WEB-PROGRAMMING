7fb6f40a58c83a2687100da77231a57b78a5ca08
Kiran Babu <kiranbabu3355@gmail.com>
2024-09-30 09:50:19 +0530

    Add files via upload

--- PHP/prime.php
+++ PHP/prime.php
@@ -12,5 +12,33 @@
         <input type="number" id="number" name="number" required>
         <button type="submit" name="submit" >Check</button>
     </form>
+
+    <?php
+    if(isset($_POST["submit"])){
+        $number= (int)$_POST['number'];
+        if(isPrime($number)){
+            print($number.' Is Prime');
+        }
+        else{
+            print($number." is not Prime" );
+        }
+    }
+
+    function isPrime($n){
+        if($n < 2){
+            return True;
+        }
+        else{
+            $ans=True;
+            for($i=2;$i<sqrt($n);$i++){
+                if($n % $i == 0){
+                    $ans=False;
+                }
+            }
+            return $ans;
+        }
+    }
+    
+    ?>
 </body>
 </html>
